77a7ab1ddecf6ed38e87a6ef3e198862d3acd762
Gabriel Pickard <wergomat@gmail.com>
2020-03-09 00:56:04 -0500

    Nikola auto commit.
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -162,7 +162,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "hack"
+THEME = "detox"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
